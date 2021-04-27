a04f41e84fc5eb2c1b14d5eb6c7ca11d15b6f668
Munod <thiagodss@hotmail.com>
2021-04-27 01:09:04 -0300

    Principal 1.1

--- main.py
+++ main.py
@@ -4,17 +4,37 @@ import PySimpleGUI as sg
 class PassGen:
     def __init__(self):
         sg.theme('Black')
+        tab1_window = [
+            [sg.T(text='Frase', size=(4, 1)), sg.In(key='Frase', size=(34, 1))],
+            [sg.Text('Quantidade de Caracteres', size=(29, 1)),
+             sg.Combo(values=list(range(8, 21)), key='Total_chars', default_value=8,
+                      size=(3, 1))],
+            [sg.Output(size=(38, 5))],
+            [sg.Button('Gerar Senha')]
+        ]
+        tab2_window = [
+            [sg.T(text=u'Digite a frase que deseja transformar em senha.\n'
+                       u'A idéia é que memorizar uma frase é melhor\nque '
+                       u'uma senha complexa.\nO programa não armazena sua'
+                       u' senha.\n\nAutor: Thiago Santos\nVersão: 1.1')]
+        ]
+        layout = [[sg.TabGroup([[sg.Tab('Principal', tab1_window), sg.Tab('Ajuda', tab2_window)]])],
+              ]
+        '''
         layout = [
-            [sg.Text('Frase', size=(10, 1)),
-             sg.Input(key='Frase', size=(20, 1))],
+            [sg.Text('Digite a frase que deseja transformar em senha.', size=(35, 1))],
+            [sg.Text('Frase', size=(4, 1)),
+             sg.Input(key='Frase', size=(33, 1))],
             [sg.Text('Quantidade de Caracteres'), sg.Combo(values=list(range(8, 21)), key='Total_chars', default_value= 8,
-                                                           size=(8, 1))],
-            [sg.Output(size=(32, 5))],
+                                                           size=(14, 1))],
+            [sg.Output(size=(37, 5))],
             [sg.Button('Gerar Senha')]
 
         ]
+        '''
         #Declarar a janela
-        self.janela = sg.Window('Gerador de Senha por Frase', layout)
+        self.janela = sg.Window('Gerador de Senha', layout)
+
     def Iniciar(self):
         while True:
             evento, valores = self.janela.read()
@@ -23,6 +43,7 @@ class PassGen:
             if evento == 'Gerar Senha':
                 nova_senha = self.Gera_senha(valores)
                 print(f'Senha: {nova_senha}, {len(nova_senha)} digitos.')
+
     def Gera_senha(self, valores):
         senha_base = valores['Frase'].encode('utf-8')
         senha_nova = base64.b64encode(senha_base).decode('utf-8')
